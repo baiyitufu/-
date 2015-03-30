c7794f3b69f2a65502c0d587f4f5af38ed79df48
featur <Add@Add.local>
2015-03-30 14:06:20 +0800

    Initial Commit

---
+++ 滑动效果/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  滑动效果
+//
+//  Created by Add on 15-3-30.
+//  Copyright (c) 2015年 shinyv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 滑动效果/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  滑动效果
+//
+//  Created by Add on 15-3-30.
+//  Copyright (c) 2015年 shinyv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
